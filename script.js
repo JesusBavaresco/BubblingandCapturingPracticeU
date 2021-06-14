cb52fd9b2d19c77cdbf9ea781025bdbc0fe11bcd
Jesus Bavaresco <chuchobavaresco@icloud.com>
2021-06-13 18:32:38 -0600

    Practice Ready

--- script.js
+++ script.js
@@ -10,6 +10,25 @@ videoBox.onclick = function() {
   videoBox.setAttribute('class','hidden');
 };
 
-video.onclick = function() {
+video.onclick = function(e) {
+  e.stopPropagation();
   video.play();
 };
+
+
+////////////////////////////// CHALLENGE EXTRA ////////////////////////////
+
+const challenge = (text) => {
+  let result = [];
+  for (let i = 0; i < text.length; i++){
+    if( result[text.charCodeAt(i)]){
+      return false;
+    }else {
+      result[text.charCodeAt(i)] = true;
+    }
+  }
+  return true;
+}
+
+let result = challenge('AaBbCc');
+console.log(result);
